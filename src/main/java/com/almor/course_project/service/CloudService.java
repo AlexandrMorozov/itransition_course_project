dbdbaf03d7c024e1f9b222b9d9eb99c65b02048a
Almor <almor9830@gmail.com>
2021-05-09 08:58:37 +0300

    Integration with cloud service(Cloudinary) added

--- src/main/java/com/almor/course_project/service/CloudService.java
+++ src/main/java/com/almor/course_project/service/CloudService.java
@@ -3,7 +3,9 @@ package com.almor.course_project.service;
 import com.cloudinary.Cloudinary;
 import com.cloudinary.utils.ObjectUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.io.File;
 import java.io.IOException;
@@ -12,24 +14,38 @@ import java.util.Map;
 @Service
 public class CloudService {
 
-    private Cloudinary cloudinary = new Cloudinary();
+    private Cloudinary cloudinary;
 
-    public void f(File file) {
+    @Autowired
+    public CloudService( @Value("${CLOUDINARY_CLOUD_NAME}") String name,
+                         @Value("${CLOUDINARY_API_KEY}") String key,
+                         @Value("${CLOUDINARY_API_SECRET}") String secret ) {
 
-        Map params = ObjectUtils.asMap(
-                "public_id", "myfolder/mysubfolder/my_dog",
-                "overwrite", true,
-                "resource_type", "image");
+        cloudinary = new Cloudinary(ObjectUtils.asMap(
+                "cloud_name", name,
+                "api_key", key,
+                "api_secret", secret));
+    }
+
+    public void loadImageToCloud(MultipartFile sourceFile) {
+
+        Map<String, Object> result = null;
 
         try {
-            Map uploadResult = cloudinary.uploader().upload(file, params);
+
+            Map<String, Object> params = ObjectUtils.asMap("resource_type", "auto");
+            result = cloudinary.uploader().upload(sourceFile.getBytes(), params);
+
         } catch (IOException e) {
 
         }
-
     }
 
+    public void deleteImageFromCloud(String publicId) {
+        try {
+            cloudinary.uploader().destroy(publicId, ObjectUtils.asMap());
+        } catch (IOException e) {
 
-
-
+        }
+    }
 }
